6960580aa75d9867eb4f40cfd5fceb58ebd33e1d
X <anmora.2396@gmail.com>
2022-04-28 22:41:12 -0600

    Updated Changes NewPage_Informes 28/04/2022

--- src/JS/Utils/dataInformes.js
+++ src/JS/Utils/dataInformes.js
@@ -50,7 +50,7 @@ const DInfoAnual = {
         {
             informes: "Informe de J.D. 2022 - SECRETARIA GENERAL",
             descripcion: "Rendición de informes de las labores realizadas para la lucha y la defensa de nuestras personas agremiadas, así como de la institución, en el período abril 2019 – abril 2022.",
-            link: "../../Documentos/Informes Anuales/Informe de J.D. 2022 - SECRETARIA GENERAL.pdf",
+            link: "../../Documentos/Informes Anuales/Informe_de_J_D_2022-SECRETARIA_GENERAL.pdf",
         },
         {
             informes: "INFORME ANUAL Fiscalía SITRAINA 2020",
@@ -61,7 +61,7 @@ const DInfoAnual = {
     OtrosInformes: [
         {
             informes: "Informe de Protocolo Control Interno Gastos SITRAINA - Propuesta Fiscalia",
-            link: "../../Documentos/protocolos/Protocolo Control Interno Gastos SITRAINA. Propuesta Fiscalia.pdf",
+            link: "../../Documentos/protocolos/Protocolo_Control_Interno_Gastos_SITRAINA_Propuesta_Fiscalia.pdf",
         },
         {
             informes: "Informe de Cheques",
@@ -69,33 +69,33 @@ const DInfoAnual = {
         },
         {
             informes: "Informe de Servicios",
-            link: "../../Documentos/gastos/Servicios LicYurlyAlguer.xlsx",
+            link: "../../Documentos/gastos/Servicios_LicYurlyAlguer.xlsx",
         },
     ],
     Documentos: [
         {
             informes: "Informe de Fiscalía Asamblea 29 de Abril",
-            link: "../../Documentos/Informes Anuales/Informe de FiscalíaAsamblea29deabril.pdf",
+            link: "../../Documentos/Informes Anuales/Informe_de_Fiscalia_Asamblea-29_de_abril.pdf",
         },
         {
             informes: "Informe de Secretaría de Afiliación y Propaganda",
-            link: "../../Documentos/Informes Anuales/Informe de Secretaría de Afiliación y Propaganda.pdf",
+            link: "../../Documentos/Informes Anuales/Informe_de_Secretaria_de_Afiliacion_y_Propaganda.pdf",
         },
         {
             informes: "Informe de Secretaría de Asuntos Culturales y Social",
-            link: "../../Documentos/Informes Anuales/Informe de Secretaría de Asuntos Culturales y Social.pdf",
+            link: "../../Documentos/Informes Anuales/Informe_de_Secretaria_de_Asuntos_Culturales_y_Social.pdf",
         },
         {
             informes: "Informe de Secretaria de Educación 2022 resumido",
-            link: "../../Documentos/Informes Anuales/informe secretaria de educ 2022 resumido.pdf",
+            link: "../../Documentos/Informes Anuales/Informe_Secretaria_de_Educacion_2022-resumido.pdf",
         },
         {
             informes: "Informe de Secretaría de la Mujer 2022",
-            link: "../../Documentos/Informes Anuales/Informe Secretaría de la Mujer 2022.pdf",
+            link: "../../Documentos/Informes Anuales/Informe_Secretaría_de_la_Mujer_2022.pdf",
         },
         {
             informes: "Informe de Secretaria General Adjunta 2022 ,28-04-22",
-            link: "../../Documentos/Informes Anuales/informe secretaria General Adjunta 2022 ,28-04-22.pdf",
+            link: "../../Documentos/Informes Anuales/Informe_Secretaria_General_Adjunta_2022-28_abril_2022.pdf",
         }
     ],
 }
