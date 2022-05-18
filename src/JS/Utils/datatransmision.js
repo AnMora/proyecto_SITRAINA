6573cf2d9f1f9d124907b02f3a7b5eaefbf21ad7
X <anmora.2396@gmail.com>
2022-05-17 22:31:42 -0600

    Update Live SI 17/05/2022

--- src/JS/Utils/datatransmision.js
+++ src/JS/Utils/datatransmision.js
@@ -26,7 +26,7 @@ const EVV = {
     ],
     Extra1: "Lo pueden ver en vivo en nuestro sitio web o en nuestra página de Facebook.",
     Extra2: "Comuníquese con nuestro departamento de comunicación.",
-    Transmision: ""
+    Transmision: "https://www.youtube.com/watch?fbclid=IwAR0xbFFkVXRwL9MeD-3C7etkd1hM5xsVimWW1pp1xQBpw0Ce6xKpt1VWzwk&v=cpuPjVQG1UA&feature=youtu.be"
 }
 
 export default EVV;
