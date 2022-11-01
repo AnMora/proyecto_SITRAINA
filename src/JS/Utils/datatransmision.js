c7b9322d8d60db4a54e0f6a96ba1ae8cc5e1a7f3
X <anmora.2396@gmail.com>
2022-11-01 17:27:35 -0600

    Updated SI 01/11/2022

--- src/JS/Utils/datatransmision.js
+++ src/JS/Utils/datatransmision.js
@@ -19,14 +19,14 @@ const EVV = {
     SITRAINAINFORMA: [
         {
             titulo: "SITRAINA INFORMA",
-            dia: "Programa: Martes 20 de Septiembre, 2022",
-            tema: "Alto Costo de la Vida y Política Nacional",
-            con: " Jorge Coronado - Dirigente del BUSSCO",
+            dia: "Programa: Martes 01 de Noviembre, 2022",
+            tema: "El INA y la lucha sindical",
+            con: " Marianela Delgado Quesada - Secretaria General Adjunta SITRAINA",
         }
     ],
     Extra1: "Lo pueden ver en vivo en nuestro sitio web o en nuestra página de Facebook.",
     Extra2: "Comuníquese con nuestro departamento de comunicación.",
-    Transmision: "https://www.facebook.com/100028989294973/videos/5483102871773040"
+    Transmision: "https://www.facebook.com/SitrainaCR/videos/890843155165426"
 }
 
 export default EVV;
